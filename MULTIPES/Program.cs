f250334934e81ab7095b3b7e390f3852ccbaef6f
atma0004 <atma0004@edu.eal.dk>
2017-02-02 09:34:12 +0100

    Add project files.

---
+++ MULTIPES/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MULTIPES
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+
+            int runningtotal = 0;
+
+            for (int i = 0; i<1000; i++)
+
+                if (i%3==0||i%9==0)
+                    {
+                    runningtotal+=i;
+                    }
+
+            Console.WriteLine("The number is {0}" , runningtotal);
+            }
+        
+
+        }
+    }
